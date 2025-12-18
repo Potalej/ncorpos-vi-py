4e5b76d5fccfa18363ef44904aa3632e945a1649
potalej <oapotalej@gmail.com>
2025-12-18 14:06:27 -0300

    ihuuu

---
+++ src/ncorpos_vi/__init__.py
@@ -0,0 +1,4 @@
+from .api import Gerador
+from ._version import __version__
+
+__all__ = ["Gerador", "__version__"]
---
+++ tests/teste.py
@@ -0,0 +1,4 @@
+import ncorpos_vi
+
+G = ncorpos_vi.Gerador(N=10, modo="sorteio_ip_direto")
+G.gerar()
