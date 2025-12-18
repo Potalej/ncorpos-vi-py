9cdea3050307fd8b3405b63b9996efe393e1fa7f
potalej <oapotalej@gmail.com>
2025-12-18 14:17:04 -0300

    ops

---
+++ repos.cmake
@@ -0,0 +1,5 @@
+# ----------------------------------------------------------------------------
+# Valores iniciais
+# ----------------------------------------------------------------------------
+set(VALORES_INICIAIS_REPO "https://github.com/potalej/ncorpos-valores-iniciais.git")
+set(VALORES_INICIAIS_TAG "main")
