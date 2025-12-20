ef61cc83ca5c9d69d78d3586e585401569e0ff17
potalej <66756864+Potalej@users.noreply.github.com>
2025-12-19 22:20:43 -0300

    Funcionando! (#1)
    * vamos la
    
    * .
    
    * Correção
    
    * teste
    
    * Teste
    
    * Vamos ver
    
    * Debug
    
    * debug
    
    * teste
    
    * teste
    
    * teste
    
    * teste
    
    * teste
    
    * Final

--- repos.cmake
+++
@@ -1,5 +0,0 @@
-# ----------------------------------------------------------------------------
-# Valores iniciais
-# ----------------------------------------------------------------------------
-set(VALORES_INICIAIS_REPO "https://github.com/potalej/ncorpos-valores-iniciais.git")
-set(VALORES_INICIAIS_TAG "main")
